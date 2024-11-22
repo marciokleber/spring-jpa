d1b36b685159099f6d6e12267322fda318fb945b
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2024-11-22 15:36:10 -0300

    First commit - Criação do projeto spring com o starter data-jpa.

---
+++ src/test/java/com/github/marciokleber/springjpa/SpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marciokleber.springjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
